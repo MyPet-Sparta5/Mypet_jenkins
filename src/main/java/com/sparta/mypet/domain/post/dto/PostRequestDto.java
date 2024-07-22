726e1929250bb334b9cdb8b91d73c1c35f7af426
GaEun1216 <gani6382@gmail.com>
2024-07-22 17:33:22 +0900

    [#20]🩹 fix : Dto 생성자, final 제거

--- src/main/java/com/sparta/mypet/domain/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/mypet/domain/post/dto/PostRequestDto.java
@@ -7,13 +7,9 @@ import lombok.Getter;
 public class PostRequestDto {
 
 	@NotBlank(message = "제목을 입력해주세요")
-	private final String title;
+	private String title;
 
 	@NotBlank(message = "내용을 입력해주세요")
-	private final String content;
+	private String content;
 
-	public PostRequestDto(String title, String content) {
-		this.title = title;
-		this.content = content;
-	}
 }
