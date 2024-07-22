0acfce461e930604e3f6f59cb0d460c73964fbc0
GaEun1216 <gani6382@gmail.com>
2024-07-22 17:28:38 +0900

    [#20]🩹 fix : @Repository 어노테이션 추가

--- src/main/java/com/sparta/mypet/domain/post/PostRepository.java
+++ src/main/java/com/sparta/mypet/domain/post/PostRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.mypet.domain.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sparta.mypet.domain.post.entity.Post;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
