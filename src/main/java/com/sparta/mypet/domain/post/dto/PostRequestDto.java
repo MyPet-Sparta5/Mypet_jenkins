31b1af3bb28e135bbf5c6b2d5d743e7605b29969
GaEun1216 <gani6382@gmail.com>
2024-07-22 14:40:53 +0900

    [#20]♻️ refactor : PostDto @Getter만 사용, 변수 final 변경

--- src/main/java/com/sparta/mypet/domain/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/mypet/domain/post/dto/PostRequestDto.java
@@ -2,16 +2,18 @@ package com.sparta.mypet.domain.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class PostRequestDto {
 
 	@NotBlank(message = "제목을 입력해주세요")
-	private String title;
+	private final String title;
 
 	@NotBlank(message = "내용을 입력해주세요")
-	private String content;
+	private final String content;
 
+	public PostRequestDto(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
 }
