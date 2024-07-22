fb1be99da9f3ef73c086907c8bac3f65842d53ff
lis0517 <01034895017a@gmail.com>
2024-07-22 17:33:37 +0900

    ♻️ refactor : 생성자 제거
    필요없는 생성자 제거

--- src/main/java/com/sparta/mypet/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/mypet/domain/comment/dto/CommentRequestDto.java
@@ -8,9 +8,5 @@ import lombok.Getter;
 @Getter
 public class CommentRequestDto {
 	@NotBlank(message = GlobalMessage.COMMENT_CONTENT_NOT_BLANK_MESSAGE)
-	private final String content;
-
-	public CommentRequestDto(String content) {
-		this.content = content;
-	}
+	private String content;
 }
