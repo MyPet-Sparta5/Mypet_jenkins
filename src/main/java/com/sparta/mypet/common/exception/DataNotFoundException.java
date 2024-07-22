9da4c55793890ee818779be01b097525f857e960
GaEun1216 <gani6382@gmail.com>
2024-07-22 14:34:36 +0900

    [#20]🔥 del : 미사용 예외처리 삭제

--- src/main/java/com/sparta/mypet/common/exception/DataNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.mypet.common.exception;
-
-import com.sparta.mypet.common.entity.GlobalMessage;
-
-public class DataNotFoundException extends RuntimeException {
-	public DataNotFoundException(String message) {
-		super(message);
-	}
-}
