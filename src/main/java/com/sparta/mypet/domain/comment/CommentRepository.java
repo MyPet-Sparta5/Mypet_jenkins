eab8d4e985af79e73ce59a3386ce7c8873b6ab5f
lis0517 <01034895017a@gmail.com>
2024-07-22 20:20:40 +0900

    ✨ feat : 댓글 페이지네이션 추가

--- src/main/java/com/sparta/mypet/domain/comment/CommentRepository.java
+++ src/main/java/com/sparta/mypet/domain/comment/CommentRepository.java
@@ -1,11 +1,12 @@
 package com.sparta.mypet.domain.comment;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sparta.mypet.domain.comment.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	List<Comment> findAllByPostId(Long postId);
+	Page<Comment> findByPostId(Long postId, Pageable pageable);
+
 }
