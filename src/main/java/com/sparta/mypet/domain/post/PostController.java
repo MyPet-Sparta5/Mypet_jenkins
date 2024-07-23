5f67d7885a2c7ead449d7ad038a1f8e52da4bbf9
hanraeul <toast17@naver.com>
2024-07-23 13:03:45 +0900

    [#8]🩹 fix : Post UserDetails -> UserDetailsImpl 변경

--- src/main/java/com/sparta/mypet/domain/post/PostController.java
+++ src/main/java/com/sparta/mypet/domain/post/PostController.java
@@ -3,7 +3,6 @@ package com.sparta.mypet.domain.post;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +17,7 @@ import com.sparta.mypet.common.dto.DataResponseDto;
 import com.sparta.mypet.common.util.ResponseFactory;
 import com.sparta.mypet.domain.post.dto.PostRequestDto;
 import com.sparta.mypet.domain.post.dto.PostResponseDto;
+import com.sparta.mypet.security.UserDetailsImpl;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -30,23 +30,23 @@ public class PostController {
 	private final PostService postService;
 
 	@PostMapping
-	public ResponseEntity<DataResponseDto<PostResponseDto>> createPost(@AuthenticationPrincipal UserDetails userDetails,
+	public ResponseEntity<DataResponseDto<PostResponseDto>> createPost(@AuthenticationPrincipal UserDetailsImpl userDetails,
 		@Valid @RequestBody PostRequestDto requestDto, @RequestParam("category") String category) {
-		PostResponseDto responseDto = postService.createPost(userDetails.getUsername(), requestDto, category);
+		PostResponseDto responseDto = postService.createPost(userDetails.getUser(), requestDto, category);
 		return ResponseFactory.created(responseDto, "게시물 생성 성공");
 	}
 
 	@PutMapping("/{postId}")
-	public ResponseEntity<DataResponseDto<PostResponseDto>> updatePost(@AuthenticationPrincipal UserDetails userDetails,
+	public ResponseEntity<DataResponseDto<PostResponseDto>> updatePost(@AuthenticationPrincipal UserDetailsImpl userDetails,
 		@Valid @RequestBody PostRequestDto requestDto, @PathVariable Long postId) {
-		PostResponseDto responseDto = postService.updatePost(userDetails.getUsername(), requestDto, postId);
+		PostResponseDto responseDto = postService.updatePost(userDetails.getUser(), requestDto, postId);
 		return ResponseFactory.ok(responseDto, "게시물 수정 성공");
 	}
 
 	@DeleteMapping("/{postId}")
-	public ResponseEntity<Void> deletePost(@AuthenticationPrincipal UserDetails userDetails,
+	public ResponseEntity<Void> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails,
 		@PathVariable Long postId) {
-		postService.deletePost(userDetails.getUsername(), postId);
+		postService.deletePost(userDetails.getUser(), postId);
 		return ResponseFactory.noContent();
 	}
 
