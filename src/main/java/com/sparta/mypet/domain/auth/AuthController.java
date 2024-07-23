fc7a8a054ae0b12157c4d412f46356cc981d68a1
seungsuuu <tmdtn013@naver.com>
2024-07-23 23:28:27 +0900

    [#31]✨ feat : 토큰 재발급 API 구현

--- src/main/java/com/sparta/mypet/domain/auth/AuthController.java
+++ src/main/java/com/sparta/mypet/domain/auth/AuthController.java
@@ -13,6 +13,7 @@ import com.sparta.mypet.common.entity.GlobalMessage;
 import com.sparta.mypet.common.util.ResponseFactory;
 import com.sparta.mypet.domain.auth.dto.LoginRequestDto;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,6 +25,7 @@ public class AuthController {
 
 	@PostMapping("/auth/login")
 	public ResponseEntity<DataResponseDto<String>> login(@RequestBody LoginRequestDto requestDto) {
+
 		String token = authService.login(requestDto);
 
 		return ResponseFactory.ok(token, GlobalMessage.LOGIN_SUCCESS.getMessage());
@@ -37,4 +39,11 @@ public class AuthController {
 		return ResponseFactory.ok(GlobalMessage.LOGOUT_SUCCESS.getMessage());
 	}
 
+	@PostMapping("/auth/refresh")
+	public ResponseEntity<DataResponseDto<String>> refreshAccessToken(HttpServletRequest request) {
+
+		String newToken = authService.refreshAccessToken(request);
+
+		return ResponseFactory.ok(newToken, GlobalMessage.REFRESH_TOKEN_SUCCESS.getMessage());
+	}
 }
