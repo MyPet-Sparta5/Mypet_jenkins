3f9015764e752418beecc8ed3cedbe7d98885d0d
seungsuuu <tmdtn013@naver.com>
2024-07-23 14:36:19 +0900

    [#28]✨ feat : logout API

--- src/main/java/com/sparta/mypet/domain/auth/AuthController.java
+++ src/main/java/com/sparta/mypet/domain/auth/AuthController.java
@@ -3,10 +3,12 @@ package com.sparta.mypet.domain.auth;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.mypet.common.dto.DataResponseDto;
+import com.sparta.mypet.common.dto.MessageResponseDto;
 import com.sparta.mypet.common.entity.GlobalMessage;
 import com.sparta.mypet.common.util.ResponseFactory;
 import com.sparta.mypet.domain.auth.dto.LoginRequestDto;
@@ -25,6 +27,14 @@ public class AuthController {
 		String token = authService.login(requestDto);
 
 		return ResponseFactory.ok(token, GlobalMessage.LOGIN_SUCCESS.getMessage());
+	}
+
+	@PostMapping("/auth/logout")
+	public ResponseEntity<MessageResponseDto> logout(@RequestHeader(value = "Authorization") String token) {
+
+		authService.logout(token);
+
+		return ResponseFactory.ok(GlobalMessage.LOGOUT_SUCCESS.getMessage());
 	}
 
 }
