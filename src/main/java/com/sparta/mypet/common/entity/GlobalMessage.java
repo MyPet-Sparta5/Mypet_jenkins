b088468a958e5d8c7291e0d86f5c4f50842578d5
seungsuuu <tmdtn013@naver.com>
2024-07-24 19:12:06 +0900

    [#29]✨ feat : 회원탈퇴 응답 메세지 추가

--- src/main/java/com/sparta/mypet/common/entity/GlobalMessage.java
+++ src/main/java/com/sparta/mypet/common/entity/GlobalMessage.java
@@ -22,6 +22,7 @@ public enum GlobalMessage {
 	LOGOUT_SUCCESS("로그아웃이 성공적으로 완료되었습니다."),
 	REFRESH_TOKEN_SUCCESS("토큰 재발급이 성공적으로 완료되었습니다."),
 	GET_USER_SUCCESS("회원 조회가 성공적으로 완료되었습니다."),
+	WITHDRAW_USER_SUCCESS("회원 탈퇴가 성공적으로 완료되었습니다."),
 
 	// 에러 메세지
 	// User
@@ -30,7 +31,8 @@ public enum GlobalMessage {
 	PASSWORD_INVALID("비밀번호가 일치하지 않습니다."),
 	USER_NOT_FOUND("존재하지 않는 유저입니다."),
 	REFRESH_INVALID("Refresh 토큰이 일치하지 않습니다."),
-	USER_STATUS_NOT_ACTIVE("해당 유저는 ACTIVE 상태가 아닙니다."),
+	USER_STATUS_WITHDRAWAL("해당 유저는 탈퇴 상태입니다."),
+	USER_STATUS_STOP("해당 유저는 정지 상태입니다."),
 
 	// Post
 	POST_NOT_FOUND("존재하지 않는 게시물입니다."),
@@ -46,7 +48,6 @@ public enum GlobalMessage {
 
 	// Valid
 	public static final String COMMENT_CONTENT_NOT_BLANK_MESSAGE = "댓글의 내용을 작성해야합니다.";
-
 
 	private final String message;
 
