0ead4a48aaeaad523fb0abb87fa444c0844cd1fd
seungsuuu <tmdtn013@naver.com>
2024-07-24 11:59:10 +0900

    [#30]✨ feat : getUser API 구현

--- src/main/java/com/sparta/mypet/domain/auth/UserController.java
+++ src/main/java/com/sparta/mypet/domain/auth/UserController.java
@@ -1,6 +1,8 @@
 package com.sparta.mypet.domain.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import com.sparta.mypet.common.entity.GlobalMessage;
 import com.sparta.mypet.common.util.ResponseFactory;
 import com.sparta.mypet.domain.auth.dto.SignupRequestDto;
 import com.sparta.mypet.domain.auth.dto.SignupResponseDto;
+import com.sparta.mypet.domain.auth.dto.UserWithPostListResponseDto;
+import com.sparta.mypet.security.UserDetailsImpl;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +32,15 @@ public class UserController {
 		SignupResponseDto responseDto = userService.signup(requestDto);
 
 		return ResponseFactory.created(responseDto, GlobalMessage.CREATE_USER_SUCCESS.getMessage());
+	}
+
+	@GetMapping("/users")
+	public ResponseEntity<DataResponseDto<UserWithPostListResponseDto>> getUser(
+		@AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+		UserWithPostListResponseDto responseDto = userService.getUser(userDetails.getUsername());
+
+		return ResponseFactory.ok(responseDto, GlobalMessage.GET_USER_SUCCESS.getMessage());
 	}
 
 }
