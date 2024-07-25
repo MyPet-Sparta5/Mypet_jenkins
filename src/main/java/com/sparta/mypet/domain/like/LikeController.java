4d3f816f4e2254c968833430bfb029b47b77122a
lis0517 <01034895017a@gmail.com>
2024-07-25 11:02:22 +0900

    🩹 fix : 코드 수정
    - @Repository 추가
    - import 제거

--- src/main/java/com/sparta/mypet/domain/like/LikeController.java
+++ src/main/java/com/sparta/mypet/domain/like/LikeController.java
@@ -2,7 +2,6 @@ package com.sparta.mypet.domain.like;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/sparta/mypet/domain/like/LikeRepository.java
+++ src/main/java/com/sparta/mypet/domain/like/LikeRepository.java
@@ -7,7 +7,9 @@ import com.sparta.mypet.domain.like.entity.Like;
 import com.sparta.mypet.domain.post.entity.Post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
 	boolean existsByUserAndPost(User user, Post post);
