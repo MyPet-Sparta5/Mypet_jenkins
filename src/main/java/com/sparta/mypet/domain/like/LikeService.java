3d1683378a307b6f23d98476668bf7766afe41e9
lis0517 <01034895017a@gmail.com>
2024-07-25 21:22:30 +0900

    ♻️ refactor : Like 리팩토링

--- src/main/java/com/sparta/mypet/domain/like/LikeService.java
+++ src/main/java/com/sparta/mypet/domain/like/LikeService.java
@@ -1,15 +1,13 @@
 package com.sparta.mypet.domain.like;
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sparta.mypet.common.entity.GlobalMessage;
-import com.sparta.mypet.common.exception.custom.PostNotFoundException;
-import com.sparta.mypet.domain.auth.UserRepository;
+import com.sparta.mypet.domain.auth.UserService;
 import com.sparta.mypet.domain.auth.entity.User;
 import com.sparta.mypet.domain.like.entity.Like;
-import com.sparta.mypet.domain.post.PostRepository;
+import com.sparta.mypet.domain.post.PostService;
 import com.sparta.mypet.domain.post.entity.Post;
 
 import lombok.RequiredArgsConstructor;
@@ -20,14 +18,15 @@ public class LikeService {
 
 	private final LikeRepository likeRepository;
 
-	private final UserRepository userRepository;
+	private final UserService userService;
 
-	private final PostRepository postRepository;
+	private final PostService postService;
 
 	@Transactional
 	public void likePost(String email, Long postId) {
-		User user = findUserByEmail(email);
-		Post post = findPostById(postId);
+
+		User user = userService.findUserByEmail(email);
+		Post post = postService.findPostById(postId);
 
 		if (likeRepository.existsByUserAndPost(user, post)) {
 			throw new IllegalStateException(GlobalMessage.LIKE_ALREADY_EXISTS.getMessage());
@@ -39,25 +38,15 @@ public class LikeService {
 
 	@Transactional
 	public void removePostLike(String email, Long postId) {
-		User user = findUserByEmail(email);
 
-		Post post = findPostById(postId);
+		User user = userService.findUserByEmail(email);
+		Post post = postService.findPostById(postId);
 
 		Like like = likeRepository.findByUserAndPost(user, post)
 			.orElseThrow(() -> new IllegalArgumentException(GlobalMessage.LIKE_NOT_FOUND.getMessage()));
 
 		post.removeLike(like);
 		likeRepository.delete(like);
-	}
-
-	private User findUserByEmail(String email) { // request to user service
-		return userRepository.findByEmail(email)
-			.orElseThrow(() -> new UsernameNotFoundException(GlobalMessage.USER_EMAIL_NOT_FOUND.getMessage()));
-	}
-
-	private Post findPostById(Long postId) { // request to post service
-		return postRepository.findById(postId)
-			.orElseThrow(() -> new PostNotFoundException(GlobalMessage.POST_NOT_FOUND.getMessage()));
 	}
 
 	private Like createAndSaveLike(User user, Post post) {
