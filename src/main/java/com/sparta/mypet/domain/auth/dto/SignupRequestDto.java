a049eae124803e4d86a6f244f59cfdca14db3d6f
seungsuuu <tmdtn013@naver.com>
2024-08-13 19:21:07 +0900

    [#192]🩹 fix : 중복되는 오류 메세지 제거

--- src/main/java/com/sparta/mypet/domain/auth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/mypet/domain/auth/dto/SignupRequestDto.java
@@ -19,9 +19,7 @@ public class SignupRequestDto {
 	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$", message = "비밀번호는 반드시 한 가지 이상의 대문자, 소문자, 숫자, 특수문자를 포함해야합니다.")
 	private String password;
 
-	@NotBlank(message = "비밀번호는 공백일 수 없습니다.")
-	@Size(min = 8, max = 15, message = "비밀번호는 8자에서 15자 사이여야 합니다.")
-	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$", message = "비밀번호는 반드시 한 가지 이상의 대문자, 소문자, 숫자, 특수문자를 포함해야합니다.")
+	@NotBlank(message = "재확인 비밀번호는 공백일 수 없습니다.")
 	private String repeatPassword;
 
 	@NotBlank(message = "이름은 공백일 수 없습니다.")
