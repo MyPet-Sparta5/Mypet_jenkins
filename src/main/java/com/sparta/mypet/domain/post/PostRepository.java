8c76d8e5d7136f1b09502293b045fb7d0b01008e
seungsuuu <tmdtn013@naver.com>
2024-08-18 16:20:41 +0900

    [#201]♻️ refactor : JPQL 쿼리 QueryDsl로 변경

--- src/main/java/com/sparta/mypet/domain/post/PostRepository.java
+++ src/main/java/com/sparta/mypet/domain/post/PostRepository.java
@@ -1,41 +1,10 @@
 package com.sparta.mypet.domain.post;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.sparta.mypet.domain.post.entity.Category;
 import com.sparta.mypet.domain.post.entity.Post;
-import com.sparta.mypet.domain.post.entity.PostStatus;
-
-import io.lettuce.core.dynamic.annotation.Param;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryQuery {
-
-	Page<Post> findByCategory(Category category, Pageable pageable);
-
-	Page<Post> findByPostStatus(PostStatus status, Pageable pageable);
-
-	Page<Post> findByCategoryAndPostStatus(Category category, PostStatus status, Pageable pageable);
-
-	@Query(value = "SELECT p.post_id " +
-		"FROM reports r " +
-		"LEFT JOIN posts p ON r.reported_post_id = p.post_id " +
-		"WHERE p.user_id = :userId",
-		nativeQuery = true)
-	List<Long> findReportedPostIdsByUserId(@Param("userId") Long userId);
-
-	@Modifying
-	@Transactional
-	@Query("UPDATE Post p SET p.postStatus = :status WHERE p.id IN :postIds AND p.id NOT IN " +
-		"(SELECT r.reportedPost.id FROM Report r WHERE r.reportStatus = 'REJECTED')")
-	void updatePostStatus(@Param("status") PostStatus status, @Param("postIds") List<Long> postIds);
-
 }
